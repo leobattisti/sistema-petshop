456c559c75b8584398ee251ef29efd930b21a6e4
Leonardo Battisti <leo.battisti1997@gmail.com>
2023-05-09 21:56:15 -0300

    Cria projeto

---
+++ src/test/java/com/leonardo/sistemapetshop/SistemaPetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.sistemapetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
